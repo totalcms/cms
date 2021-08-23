0a42b2a711f4c92ccce407c7c0381d74ae87fe4f
odan <Frisco#2018>
2021-08-23 14:46:36 +0200

    Add phpunit test

---
+++ tests/TestCase/DemoTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Test\TestCase;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test.
+ */
+final class DemoTest extends TestCase
+{
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
