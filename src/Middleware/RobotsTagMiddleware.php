642371fc1d8574e472913d6c36927fb86f3805a6
odan <Frisco#2018>
2021-08-23 17:36:18 +0200

    Add RobotsTag middleware

---
+++ src/Middleware/RobotsTagMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Allow indexing of images.
+ */
+final class RobotsTagMiddleware implements MiddlewareInterface
+{
+    /**
+     * Invoke middleware.
+     *
+     * @param ServerRequestInterface $request The request
+     * @param RequestHandlerInterface $handler The handler
+     *
+     * @return ResponseInterface The response
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        return $response->withoutHeader('X-Robots-Tag');
+    }
+}
