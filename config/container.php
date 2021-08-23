50402794b82b8837292bbdb4f3ae48a8cd754089
odan <d.opitz@outlook.com>
2021-08-23 22:22:15 +0200

    Clean up container

--- config/container.php
+++ config/container.php
@@ -9,22 +9,11 @@ use Selective\BasePath\BasePathMiddleware;
 use Selective\Config\Configuration;
 use Selective\Validation\Encoder\JsonEncoder;
 use Selective\Validation\Middleware\ValidationExceptionMiddleware;
+use Selective\Validation\Transformer\ErrorDetailsResultTransformer;
 use Slim\App;
 use Slim\Factory\AppFactory;
 use Slim\Interfaces\RouteParserInterface;
 use Slim\Middleware\ErrorMiddleware;
-use Slim\Psr7\Factory\UriFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigExtension;
-use Slim\Views\TwigMiddleware;
-use Slim\Views\TwigRuntimeLoader;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
-use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
-use Twig\Loader\FilesystemLoader;
-use Twig\TwigFunction;
 
 return [
     // Application settings
@@ -58,96 +47,16 @@ return [
         return new FilesystemIteratorFactory($container->get(Configuration::class)->getString('datadir'));
     },
 
-    TwigMiddleware::class => function (ContainerInterface $container) {
-        return TwigMiddleware::createFromContainer($container->get(App::class), Twig::class);
-    },
-
-    // Twig templates
-    Twig::class => function (ContainerInterface $container) {
-        $config = $container->get(Configuration::class);
-        $settings = $config->getArray('twig');
-
-        $options = $settings['options'];
-        $options['cache'] = $options['cache_enabled'] ? $options['cache_path'] : false;
-
-        $twig = Twig::create($settings['paths'], $options);
-
-        $loader = $twig->getLoader();
-        if ($loader instanceof FilesystemLoader) {
-            $loader->addPath($config->getString('public'), 'public');
-        }
-
-        // Add extensions
-        // $twig->addExtension(new TranslationExtension($container->get(Translator::class)));
-        // $twig->addExtension(new WebpackExtension(
-        //     $config->getString('public') . '/assets/manifest.json',
-        //     'assets/',
-        //     'assets/'
-        // ));
-
-        // Add the Twig extension only if we run the application from the command line / cron job,
-        // but not when phpunit tests are running.
-        if ((PHP_SAPI === 'cli' || PHP_SAPI === 'cgi-fcgi') && !defined('PHPUNIT_TEST_SUITE')) {
-            $app = $container->get(App::class);
-            $routeParser = $app->getRouteCollector()->getRouteParser();
-            $uri = (new UriFactory())->createUri('http://localhost');
-
-            $runtimeLoader = new TwigRuntimeLoader($routeParser, $uri);
-            $twig->addRuntimeLoader($runtimeLoader);
-            $twig->addExtension(new TwigExtension());
-        }
-
-        /** @var FlashBagInterface $flashbag */
-        $flashbag = $container->get(Session::class)->getFlashBag();
-        $environment = $twig->getEnvironment();
-        $environment->addGlobal('flashbag', $flashbag);
-        $environment->addFunction(new TwigFunction(
-            'flash',
-            function (string $key, $default = null) use ($flashbag) {
-                return $flashbag->get($key, $default ?? [])[0] ?? null;
-            }
-        ));
-
-        return $twig;
-    },
-
-    Session::class => function (ContainerInterface $container) {
-        $settings = $container->get(Configuration::class)->getArray('session');
-
-        if (PHP_SAPI === 'cli') {
-            return new Session(new MockArraySessionStorage());
-        }
-
-        return new Session(new NativeSessionStorage($settings));
-    },
-
-    SessionInterface::class => function (ContainerInterface $container) {
-        return $container->get(Session::class);
-    },
-
     BasePathMiddleware::class => function (ContainerInterface $container) {
         $app = $container->get(App::class);
 
         return new BasePathMiddleware($app);
     },
 
-    // Database connection
-    // Connection::class => function (ContainerInterface $container) {
-    //     return new Connection($container->get(Configuration::class)->getArray('db'));
-    // },
-
-    // PDO::class => function (ContainerInterface $container) {
-    //     $db = $container->get(Connection::class);
-    //     $driver = $db->getDriver();
-    //     $driver->connect();
-
-    //     return $driver->getConnection();
-    // },
-
     ValidationExceptionMiddleware::class => function (ContainerInterface $container) {
         $factory = $container->get(ResponseFactoryInterface::class);
 
-        return new ValidationExceptionMiddleware($factory, new JsonEncoder());
+        return new ValidationExceptionMiddleware($factory, new ErrorDetailsResultTransformer(), new JsonEncoder());
     },
 
     ErrorMiddleware::class => function (ContainerInterface $container) {
