96195de9695352dd07519bfe192c8faa481d20c5
Daniel Opitz <d.opitz@outlook.com>
2021-09-08 18:43:52 +0200

    Add action tests

---
+++ tests/TestCase/Action/Schema/SchemaSaveActionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Test\TestCase\Action\Schema;
+
+use App\Test\Traits\AppTestTrait;
+use Fig\Http\Message\StatusCodeInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test.
+ *
+ * @coversDefaultClass \App\Action\Collection\Schema\SchemaSaveAction
+ */
+final class SchemaSaveActionTest extends TestCase
+{
+    use AppTestTrait;
+
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function testInvoke(): void
+    {
+        //file_put_contents($this->path . '')
+        $data = [
+            'title' => 'test',
+            'description' => 'test description',
+            'type' => 'object',
+            'index' => [],
+            'required' => [],
+            'properties' => [
+                'key' => 'value',
+            ],
+        ];
+
+        $url = $this->urlFor('schema-save', ['collection' => 'test']);
+        $request = $this->createJsonRequest('POST', $url, $data);
+        $response = $this->app->handle($request);
+
+        $expected = [
+            'data' => [
+                'title' => 'test',
+                'description' => 'test description',
+                'type' => 'object',
+                'index' => [],
+                'required' => [],
+                'properties' => [
+                    'key' => 'value',
+                ],
+            ],
+        ];
+
+        $this->assertJsonData($expected, $response);
+        $this->assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
+    }
+}
---
+++ tests/Traits/AppTestTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Test\Traits;
+
+use App\Domain\Storage\Filesystem;
+use org\bovigo\vfs\vfsStream;
+use Selective\TestTrait\Traits\ArrayTestTrait;
+use Selective\TestTrait\Traits\ContainerTestTrait;
+use Selective\TestTrait\Traits\HttpJsonTestTrait;
+use Selective\TestTrait\Traits\HttpTestTrait;
+use Selective\TestTrait\Traits\MockTestTrait;
+use Slim\App;
+
+/**
+ * App Test Trait.
+ */
+trait AppTestTrait
+{
+    use ArrayTestTrait;
+    use ContainerTestTrait;
+    use HttpTestTrait;
+    use HttpJsonTestTrait;
+    use MockTestTrait;
+    use RouteTestTrait;
+
+    protected App $app;
+
+    protected string $path;
+
+    protected Filesystem $filesystem;
+
+    /**
+     * Before each test.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->app = require __DIR__ . '/../../config/bootstrap.php';
+        $this->setUpContainer($this->app->getContainer());
+
+        // Mock filesystem
+        $this->path = vfsStream::setup()->url();
+
+        if (method_exists($this->container, 'set')) {
+            $this->container->set(Filesystem::class, new Filesystem($this->path));
+        }
+
+        $this->filesystem = $this->container->get(Filesystem::class);
+    }
+}
