3c09bd338aba9e16e97351c5084079a1952075fd
Joe Workman <joe@workmanmail.com>
2023-05-25 23:03:42 -0700

    fixed docs URL

--- config/routes/docs.php
+++ config/routes/docs.php
@@ -1,11 +1,13 @@
 <?php
 
 use Slim\App;
+use TotalCMS\Action\OpenApi\DocIndexAction;
+use TotalCMS\Action\OpenApi\DocVersion1Action;
 
 return function (App $app) {
     // Redirect to Swagger documentation
-    $app->get('/docs', \App\Action\OpenApi\DocIndexAction::class);
+    $app->get('/docs', DocIndexAction::class);
 
     // Swagger API documentation
-    $app->get('/docs/v1', \App\Action\OpenApi\DocVersion1Action::class)->setName('docs');
+    $app->get('/docs/v1', DocVersion1Action::class)->setName('docs');
 };
