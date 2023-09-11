b7ddf484da5806d0689bd4a86975daeef40c7d45
Joe Workman <joe@workmanmail.com>
2023-09-11 14:11:57 -0700

    collection tests

---
+++ tests/Feature/CollectionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use function Nekofar\Slim\Pest\postJson;
+
+function collectionTestData(): array
+{
+    $json = file_get_contents(__DIR__ . '/../test-data/new-collection.json');
+
+    return json_decode($json, true);
+}
+
+beforeEach(function (): void {
+    $app = require __DIR__ . '/../../config/bootstrap.php';
+    $this->setUpApp($app);
+});
+
+it('saves a new collection', function (): void {
+    $collection = collectionTestData();
+    $id         = $collection['id'];
+    postJson('/collections', $collection)
+        ->assertOk()
+        ->assertJson()
+        ->assertJsonFragment([
+            'id' => $id,
+        ]);
+
+    $this->assertFileExists(__DIR__ . "/../tcms-data/.schemas/{$id}.json");
+});
