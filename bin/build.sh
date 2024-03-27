2c8c70f46dc094db7d662b5110a07090d9a7db7c
Joe Workman <joe@workmanmail.com>
2024-03-27 15:28:18 -0700

    quiet composer on build

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Building the application..."
+
+echo "Building frontend assets..."
 yarn build
 
-composer install --no-dev --optimize-autoloader
+echo "Building the application..."
+composer install --no-dev --optimize-autoloader --quiet
 
 # remove imagine libs that are not required and take up too much space
 find vendor -not -name '*php' -not -name '*pem' -type f -delete
@@ -12,9 +16,11 @@ find vendor -name bin -type d | xargs rm -rf
 find vendor -name test -type d | xargs rm -rf
 
 # move required files to dist
+echo "Moving required files to dist..."
 rm -rf dist
 mkdir dist
 cp -r config public resources schemas src templates vendor autoload.php dist
 
 # install all required composer packages for dev environment
-composer install
+echo "Installing all required composer packages back for dev environment..."
+composer install --quiet
