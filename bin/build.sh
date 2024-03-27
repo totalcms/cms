5ae066dd6fa9b9cde7e7514806f4b9922346c557
Joe Workman <joe@workmanmail.com>
2024-03-27 15:21:17 -0700

    build script for distribution

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yarn build
+
 composer install --no-dev --optimize-autoloader
 
 # remove imagine libs that are not required and take up too much space
@@ -8,3 +10,11 @@ find vendor -name "*phpstorm*" -delete
 find vendor -empty -type d -delete
 find vendor -name bin -type d | xargs rm -rf
 find vendor -name test -type d | xargs rm -rf
+
+# move required files to dist
+rm -rf dist
+mkdir dist
+cp -r config public resources schemas src templates vendor autoload.php dist
+
+# install all required composer packages for dev environment
+composer install
