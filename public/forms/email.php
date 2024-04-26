530aba4c429dbf93483dd00ad8a93f91b97ecb41
Joe Workman <joe@workmanmail.com>
2024-04-25 19:38:05 -0700

    Email form + typos

--- public/forms/email.php
+++ public/forms/email.php
@@ -1,13 +1,10 @@
 <?php include __DIR__ . '/_start.php'; ?>
 
-	<h1>Total CMS Toggle Form Demo</h1>
+	<h1>Total CMS Email Form Demo</h1>
 
 	{% import "totalform.twig" as form %}
 
-	{{ form.checkboxForm('mycheck') }}
-
-	{{ form.toggleForm('mytoggle') }}
-
+	{{ form.emailForm('myemail') }}
 
 	<button class="cms-save">Save</button>
 
--- public/forms/svg.php
+++ public/forms/svg.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/_start.php'; ?>
 
-	<h1>Total CMS Number Form Demo</h1>
+	<h1>Total CMS SVG Form Demo</h1>
 
 	{% import "totalform.twig" as form %}
 
