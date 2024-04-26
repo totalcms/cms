12ccee886f53e0d8dfa9f4c5c01fba9962f287c2
Joe Workman <joe@workmanmail.com>
2024-04-25 19:31:11 -0700

    toggle forms

---
+++ public/forms/toggle.php
@@ -0,0 +1,14 @@
+<?php include __DIR__ . '/_start.php'; ?>
+
+	<h1>Total CMS Number Form Demo</h1>
+
+	{% import "totalform.twig" as form %}
+
+	{{ form.checkboxForm('mycheck') }}
+
+	{{ form.toggleForm('mytoggle') }}
+
+
+	<button class="cms-save">Save</button>
+
+<?php include __DIR__ . '/_end.php'; ?>
