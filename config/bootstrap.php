3c4e4cba580a26ea64e4eb56bd68c731c77bb53f
Joe Workman <joe@workmanmail.com>
2024-04-30 17:27:18 -0700

    middleware first

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -26,10 +26,10 @@ $container = new Container(require __DIR__ . '/container.php');
 // Create App instance
 $app = $container->get(App::class);
 
-// Register routes
-(require __DIR__ . '/routes.php')($app);
-
 // Register middleware
 (require __DIR__ . '/middleware.php')($app);
+
+// Register routes
+(require __DIR__ . '/routes.php')($app);
 
 return $app;
