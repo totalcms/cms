50aac35b5fa4e71ac2a06f446c752a47c7b6ca61
Joe Workman <joe@workmanmail.com>
2024-05-14 09:24:59 -0700

    fix preview stylesheets for builder and docs

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,15 +7,9 @@ use Slim\App;
 if (php_sapi_name() == 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = basename($_SERVER['SCRIPT_FILENAME']);
     $file                   = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
     if (file_exists(__DIR__ . $file)) {
         /* Return contents of the static file. */
         return false;
-    }
-
-    if (str_contains($_SERVER['DOCUMENT_ROOT'], 'RapidWeaver') || str_contains($_SERVER['DOCUMENT_ROOT'], 'Stacks')) {
-        // Stacks Internal PHP Preview server
-        $_SERVER['APP_ENV'] = 'preview';
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -6,6 +6,31 @@ if (php_sapi_name() == 'cli-server') {
     if (is_string($path) && file_exists($path)) {
         return false;
     }
+
+    // Stacks Internal PHP Preview server
+    if (str_contains($_SERVER['DOCUMENT_ROOT'], 'RapidWeaver') || str_contains($_SERVER['DOCUMENT_ROOT'], 'Stacks')) {
+        $_SERVER['APP_ENV'] = 'preview';
+    }
+    if ($_GET['route']) {
+        $path = __DIR__ . $_GET['route'];
+        if (file_exists($path)) {
+            $ext  = pathinfo($path, PATHINFO_EXTENSION);
+            switch ($ext) {
+                case 'css':
+                    $mime = 'text/css';
+                    break;
+                case 'js':
+                    $mime = 'application/javascript';
+                    break;
+                default:
+                    $mime = mime_content_type($path);
+            }
+
+            header('Content-Type: ' . $mime);
+            echo file_get_contents($path);
+            exit;
+        }
+    }
 }
 
 (require __DIR__ . '/../config/bootstrap.php')->run();
