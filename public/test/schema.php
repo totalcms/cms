47d48dc0795ea7b9ec6b81250e3ec7590e1ae874
Joe Workman <joe@workmanmail.com>
2024-06-24 22:07:46 -0700

    schema form first draft

---
+++ public/test/schema.php
@@ -0,0 +1,9 @@
+<?php include __DIR__ . '/_start.php'; ?>
+
+	<h1>Total CMS Schema Form Demo</h1>
+
+	{% import "totalform.twig" as form %}
+
+	{{ form.schemaForm() }}
+
+<?php include __DIR__ . '/_end.php'; ?>
