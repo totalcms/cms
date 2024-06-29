8239a1452b4da09bfd52ab1134c578d7be3c3f67
Joe Workman <joe@workmanmail.com>
2024-06-29 07:44:38 -0700

    phpcsfixer

--- src/Domain/Twig/TwigEngine.php
+++ src/Domain/Twig/TwigEngine.php
@@ -15,56 +15,56 @@ use Twig\Runtime\EscaperRuntime;
  */
 final class TwigEngine
 {
-	private TwigEnvironment $twig;
+    private TwigEnvironment $twig;
 
-	public function __construct(Config $config, TotalCMSTwigExtension $extension)
-	{
-		$internalTemplates = TemplateRepository::RESERVED_TEMPLATE_DIR;
-		$customTemplates   = $config->dataDir . '/' . TemplateRepository::CUSTOM_TEMPLATE_DIR;
-		$cacheDir          = $config->cacheDir === 'false' ? false : $config->cacheDir;
-		$debug             = $cacheDir === false ? true : false;                        // enable debug is no cache dir
+    public function __construct(Config $config, TotalCMSTwigExtension $extension)
+    {
+        $internalTemplates = TemplateRepository::RESERVED_TEMPLATE_DIR;
+        $customTemplates   = $config->dataDir . '/' . TemplateRepository::CUSTOM_TEMPLATE_DIR;
+        $cacheDir          = $config->cacheDir === 'false' ? false : $config->cacheDir;
+        $debug             = $cacheDir === false ? true : false;                        // enable debug is no cache dir
 
-		if (!file_exists($internalTemplates)) {
-			throw new \DomainException("Internal templates directory not found: $internalTemplates");
-		}
-		$paths = [$internalTemplates];
-		if (file_exists($customTemplates)) {
-			$paths[] = $customTemplates;
-		}
+        if (!file_exists($internalTemplates)) {
+            throw new \DomainException("Internal templates directory not found: $internalTemplates");
+        }
+        $paths = [$internalTemplates];
+        if (file_exists($customTemplates)) {
+            $paths[] = $customTemplates;
+        }
 
-		$loader     = new TwigFilesystemLoader($paths);
-		$this->twig = new TwigEnvironment($loader, [
-			'cache' => $cacheDir,
-			'debug' => $debug,
-		]);
-		$this->twig->addExtension($extension);
-		if ($debug) {
-			$this->twig->addExtension(new DebugExtension());
-		}
+        $loader     = new TwigFilesystemLoader($paths);
+        $this->twig = new TwigEnvironment($loader, [
+            'cache' => $cacheDir,
+            'debug' => $debug,
+        ]);
+        $this->twig->addExtension($extension);
+        if ($debug) {
+            $this->twig->addExtension(new DebugExtension());
+        }
 
-		// !BUG: this is not working: https://github.com/twigphp/Twig/issues/4113
-		$this->twig->getRuntime(EscaperRuntime::class)->setSafeClasses([
-			TotalForm::class           => ['html'],
-			TotalCMSTwigAdapter::class => ['html'],
-		]);
-	}
+        // !BUG: this is not working: https://github.com/twigphp/Twig/issues/4113
+        $this->twig->getRuntime(EscaperRuntime::class)->setSafeClasses([
+            TotalForm::class           => ['html'],
+            TotalCMSTwigAdapter::class => ['html'],
+        ]);
+    }
 
-	public function render(string $templateName, array $data = []): string
-	{
-		try {
-			$string = $this->twig->render($templateName, $data);
-		} catch (\Exception $e) {
-			// throw new \DomainException("Error rendering template: $templateName - " . $e->getMessage());
-			$string = "<!-- Error rendering template: $templateName - " . $e->getMessage() . '-->';
-		}
+    public function render(string $templateName, array $data = []): string
+    {
+        try {
+            $string = $this->twig->render($templateName, $data);
+        } catch (\Exception $e) {
+            // throw new \DomainException("Error rendering template: $templateName - " . $e->getMessage());
+            $string = "<!-- Error rendering template: $templateName - " . $e->getMessage() . '-->';
+        }
 
-		return $string;
-	}
+        return $string;
+    }
 
-	public function renderString(string $template, array $data = []): string
-	{
-		$twig = $this->twig->createTemplate($template);
+    public function renderString(string $template, array $data = []): string
+    {
+        $twig = $this->twig->createTemplate($template);
 
-		return $twig->render($data);
-	}
+        return $twig->render($data);
+    }
 }
