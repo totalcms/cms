d6e792edaebfcba46a370e3b385f47d1ed06053f
Joe Workman <joe@workmanmail.com>
2024-07-03 23:50:51 -0700

    time field

---
+++ public/test/time.php
@@ -0,0 +1,15 @@
+<?php include __DIR__ . '/_start.php'; ?>
+
+<h1>Total CMS Time Form Demo</h1>
+
+{% set numberform = cms.form.builder('text', {
+	id     : 'mytime',
+	hideID : true,
+}) %}
+
+{{ numberform.addField('text', { field:'time' }) }}
+
+{{ numberform.build() }}
+
+
+<?php include __DIR__ . '/_end.php'; ?>
--- src/Domain/Admin/FormField/TimeField.php
+++ src/Domain/Admin/FormField/TimeField.php
@@ -4,4 +4,6 @@ namespace TotalCMS\Domain\Admin\FormField;
 
 final class TimeField extends FormField
 {
+	protected string $defaultFieldType = 'time';
+	protected string $defaultInputType = 'time';
 }
