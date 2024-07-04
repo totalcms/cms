511c70537a46b2b79ec3fa37dd8f98c14ad31b29
Joe Workman <joe@workmanmail.com>
2024-07-03 22:23:28 -0700

    password field

--- src/Domain/Admin/FormField/PasswordField.php
+++ src/Domain/Admin/FormField/PasswordField.php
@@ -4,4 +4,6 @@ namespace TotalCMS\Domain\Admin\FormField;
 
 final class PasswordField extends FormField
 {
+	protected string $defaultInputType = 'password';
+	protected string $defaultFieldType = 'password';
 }
