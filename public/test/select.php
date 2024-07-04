16c23a5cd2fae429fa4617b67dab9dc3aff3bb5d
Joe Workman <joe@workmanmail.com>
2024-07-03 19:00:53 -0700

    full custom select form example

--- public/test/select.php
+++ public/test/select.php
@@ -6,6 +6,29 @@
 	delete : "Clear Value"
 }) }}
 
+{% set selectform = cms.form.builder({
+	id         : 'customselect',
+	collection : 'text',
+	hideID     : true,
+}) %}
+
+{{ selectform.addField('text', {
+	label       : 'Select Something',
+	help        : 'Choose your selection here.',
+	placeholder : 'Click to Choose Something',
+	field       : 'select',
+	options     : [
+		{ "value" : "1", "label" : "Custom Option 1" },
+		{ "value" : "2", "label" : "Custom Option 2" },
+		{ "value" : "3", "label" : "Custom Option 3" },
+		{ "value" : "4", "label" : "Custom Option 4" },
+		{ "value" : "5", "label" : "Custom Option 5" },
+		{ "value" : "6", "label" : "Custom Option 6" }
+	]
+}) }}
+{{ selectform.build() }}
+
+
 <!--
 The Select options can be defined inside of the collection meta
 
