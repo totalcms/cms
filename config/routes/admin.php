06659fb9f03d95f43358867b426b07edcb04e476
Joe Workman <joe@workmanmail.com>
2024-07-21 16:41:29 -0700

    start admin routes

--- config/routes/admin.php
+++ config/routes/admin.php
@@ -7,5 +7,22 @@ use TotalCMS\Action\Admin\AdminIndexAction;
 return function (App $app) {
 	// Display Admin Interface
 	$app->get('/admin', AdminIndexAction::class)->setName('admin-index');
+
+	$app->get('/admin/schemas', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/schemas/{schema}', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/schemas/{schema}/new', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/schemas/{schema}/{id}', AdminIndexAction::class)->setName('admin-index');
+
+	$app->get('/admin/collections', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/collections/new', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/collections/{collection}/edit', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/collections/{collection}', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/collections/{collection}/add', AdminIndexAction::class)->setName('admin-index');
+	$app->get('/admin/collections/{collection}/{id}', AdminIndexAction::class)->setName('admin-index');
+
+	$app->get('/admin/docs', AdminImageworksAction::class)->setName('imageworks');
+	$app->get('/admin/logs', AdminImageworksAction::class)->setName('imageworks');
+	$app->get('/admin/settings', AdminImageworksAction::class)->setName('imageworks');
+
 	$app->get('/admin/imageworks', AdminImageworksAction::class)->setName('imageworks');
 };
