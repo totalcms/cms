e95b2266eb014e544c1d0948f83faf91d0881bdc
Joe Workman <joe@workmanmail.com>
2024-07-25 17:46:21 -0700

    collection table JS object

--- javascript/admin.js
+++ javascript/admin.js
@@ -3,8 +3,7 @@ import TotalCMS from './totalcms';
 import FactoryForm from './totalform/factory';
 import Scrollable from './totalform/scrollable';
 import AdminList from './totalform/admin-list';
-import { Grid, html } from "gridjs";
-// import { RowSelection } from "gridjs/plugins/selection";
+import CollectionTable from './totalform/collection-table';
 
 globalThis.TotalCMS = TotalCMS;
 
@@ -21,51 +20,5 @@ document.addEventListener("DOMContentLoaded", event => {
 	adminlists.forEach(list => new AdminList(list));
 
 	const tables = Array.from(document.getElementsByClassName("collection-table"));
-	tables.forEach(table => {
-		const collection = table.dataset.collection;
-
-		const wrapper = document.createElement("div");
-		wrapper.classList.add("collection-table-wrapper");
-		table.parentNode.insertBefore(wrapper, table);
-
-		// const headers = Array.from(table.getElementsByTagName("th"));
-		// headers.map(header => {name : html(header.innerHTML)});
-
-		// console.log(headers);
-
-		// headers.unshift({
-		// 	name: "Select",
-		// 	plugin: {
-		// 		component: RowSelection,
-		// 	},
-		// });
-
-		const grid = new Grid({
-			// columns    : headers,
-			from       : table,
-			pagination : {
-				limit   : 25,
-			},
-			search      : true,
-			sort        : true,
-			resizable   : true,
-			autoWidth   : true,
-			fixedHeader : true,
-			language    : {
-				search: {
-					placeholder : "Filter objects",
-				},
-			},
-		});
-		grid.render(wrapper);
-		grid.on('rowClick', e => {
-			const row = e.currentTarget;
-			const cells = Array.from(row.querySelectorAll("td"));
-			let id = '';
-			cells.forEach(cell => {
-				if (cell.dataset.columnId === 'id') id = cell.innerText;
-			});
-			window.location.href = `${window.location.href}/${id}`;
-		});
-	});
+	tables.forEach(table => new CollectionTable(table));
 });
