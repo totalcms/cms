b07d88ba6d49323ea1a3df6af6476d7fa96786f2
Joe Workman <joe@workmanmail.com>
2024-08-07 06:37:12 -0700

    filter null values from collection

--- src/Domain/Admin/TotalForm.php
+++ src/Domain/Admin/TotalForm.php
@@ -261,7 +261,8 @@ abstract class TotalForm
 			return [];
 		}
 
-		return $collection->objects->pluck($property)->flatten()->unique()->toArray();
+		// array_filter removes any empty values
+		return array_filter($collection->objects->pluck($property)->flatten()->unique()->toArray());
 	}
 
 	/**
