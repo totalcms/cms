a3038d9078e7afd0aa84424fa454896277cc9edc
Joe Workman <joe@workmanmail.com>
2024-09-23 18:10:08 -0700

    auth user login count

--- src/Domain/Auth/Service/LastLoginUpdateService.php
+++ src/Domain/Auth/Service/LastLoginUpdateService.php
@@ -3,15 +3,22 @@
 namespace TotalCMS\Domain\Auth\Service;
 
 use TotalCMS\Domain\Object\Service\ObjectSaver;
+use TotalCMS\Domain\Property\Service\PropertyFetcher;
 
 final class LastLoginUpdateService
 {
 	public function __construct(
 		private ObjectSaver $objectSaver,
+		private PropertyFetcher $propertyFetcher,
 	) {}
 
 	public function updateLoginDate(string $collection, string $id): void
 	{
-		$this->objectSaver->patchObject($collection, $id, ['lastlogin' => date('c')]);
+		$count = intval((string)$this->propertyFetcher->fetchProperty($collection, $id, 'count'));
+
+		$this->objectSaver->patchObject($collection, $id, [
+			'lastlogin' => date('c'),
+			'count'     =>  $count + 1,
+		]);
 	}
 }
