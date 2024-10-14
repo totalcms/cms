58bb29f66d39281662fd24c90f2afacf1014315f
Joe Workman <joe@workmanmail.com>
2024-10-13 23:46:21 -0700

    file chunk logic

--- src/Action/Property/File/FileSaveAction.php
+++ src/Action/Property/File/FileSaveAction.php
@@ -60,3 +60,59 @@ final class FileSaveAction
 		return $this->renderer->jsonItem($response, $object, new ObjectMetaTransformer());
 	}
 }
+
+// public function chunk(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+// {
+//     $files = $request->getUploadedFiles();
+//     $file  = $files[$args['property']];
+
+//     // Get chunk information from the request
+//     $chunkIndex = $request->getParsedBody()['chunkIndex'] ?? 0;
+//     $totalChunks = $request->getParsedBody()['totalChunks'] ?? 1;
+//     $originalFilename = $file->getClientFilename();
+//     $chunkFilename = $this->config->tmpdir . '/' . $originalFilename . '.part' . $chunkIndex;
+
+//     // Ensure the temporary directory exists
+//     if (!file_exists($this->config->tmpdir)) {
+//         mkdir($this->config->tmpdir, 0777, true);
+//     }
+
+//     // Move the uploaded chunk to the temporary directory
+//     $file->moveTo($chunkFilename);
+
+//     // If this is the last chunk, assemble the file
+//     if ($chunkIndex == $totalChunks - 1) {
+//         $finalFilePath = $this->config->tmpdir . '/' . $originalFilename;
+//         $finalFile = fopen($finalFilePath, 'wb');
+
+//         // Assemble the chunks
+//         for ($i = 0; $i < $totalChunks; $i++) {
+//             $chunkPath = $this->config->tmpdir . '/' . $originalFilename . '.part' . $i;
+//             $chunk = fopen($chunkPath, 'rb');
+//             while ($data = fread($chunk, 8192)) {
+//                 fwrite($finalFile, $data);
+//             }
+//             fclose($chunk);
+//             unlink($chunkPath); // Delete the chunk after appending
+//         }
+
+//         fclose($finalFile);
+
+//         // Save the assembled file
+//         $object = $this->service->saveFile(
+//             $args['collection'],
+//             $args['id'],
+//             $args['property'],
+//             $finalFilePath,
+//         );
+
+//         if (!$object instanceof ObjectData) {
+//             throw new \RuntimeException('Unable to collect object data from saved file');
+//         }
+
+//         return $this->renderer->jsonItem($response, $object, new ObjectMetaTransformer());
+//     }
+
+//     // If not the last chunk, return a success response
+//     return $response->withStatus(200)->withHeader('Content-Type', 'application/json')->write(json_encode(['status' => 'chunk received']));
+// }
