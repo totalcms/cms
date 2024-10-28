7b623a847b5090ebff997d9527139948a657542a
Joe Workman <joe@workmanmail.com>
2024-10-27 23:45:17 -0700

    macro builder JS

--- javascript/filelinks.js
+++ javascript/filelinks.js
@@ -1,59 +1,12 @@
+import MacroBuilder from "./macro-builder";
+
 if (window.self !== window.top) {
 	// The page is in an iframe
 	document.body.classList.add('in-iframe');
 }
 
-document.addEventListener("DOMContentLoaded", event => {
-
-	const generateTwigMacro = (data) => {
-		const id         = data.id;
-		const collection = data.collection;
-		const property   = data.property;
-		const name       = data.name;
-
-		// Delete the keys from data
-		delete data.id;
-		delete data.collection;
-		delete data.property;
-		delete data.cache;
-		delete data.name;
-
-		// Convert string numbers to actual numbers
-		data = Object.entries(data).reduce((acc, [key, value]) => {
-			acc[key] = isNaN(value) ? value : parseFloat(value);
-			return acc;
-		}, {});
-
-		let options = '';
-
-		if (Object.keys(data).length > 0) {
-			options = JSON.stringify(data);
-			options = options.replace(/"(\w+)"\s*:/g, '$1:').trim();
-			options = `, ${options}`;
-		}
-
-		let macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-		if (property === "image") {
-			macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}'}) }}`;
-
-			if (collection === "image") {
-				macro = `{{ cms.imagePath('${id}'${options}) }}`;
-			}
-		}
-
-		if (name) {
-			macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-			if (property === "gallery") {
-				macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}'}) }}`;
-
-				if (collection === "gallery") {
-					macro = `{{ cms.galleryPath('${id}', '${name}'${options}) }}`;
-				}
-			}
-		}
-
-		const macroContent = document.getElementById("twig-macro");
-		macroContent.textContent = macro;
-	}
-	generateTwigMacro(getFormData());
-});
+const generateTwigMacro = (data) => {
+	const macroContent = document.getElementById("twig-macro");
+	macroContent.textContent = MacroBuilder.fileDownloadMacro(data);
+}
+generateTwigMacro(getFormData());
--- javascript/imageworks-builder.js
+++ javascript/imageworks-builder.js
@@ -1,5 +1,7 @@
+import MacroBuilder from "./macro-builder";
 import Details from "./totalform/details";
 import Dialog from "./totalform/dialog";
+import MacroBuilder from "./macro-builder";
 
 if (window.self !== window.top) {
 	// The page is in an iframe
@@ -32,54 +34,8 @@ document.addEventListener("DOMContentLoaded", event => {
 	});
 
 	const generateTwigMacro = (data) => {
-		const id         = data.id;
-		const collection = data.collection;
-		const property   = data.property;
-		const name       = data.name;
-
-		// Delete the keys from data
-		delete data.id;
-		delete data.collection;
-		delete data.property;
-		delete data.cache;
-		delete data.name;
-
-		// Convert string numbers to actual numbers
-		data = Object.entries(data).reduce((acc, [key, value]) => {
-			acc[key] = isNaN(value) ? value : parseFloat(value);
-			return acc;
-		}, {});
-
-		let options = '';
-
-		if (Object.keys(data).length > 0) {
-			options = JSON.stringify(data);
-			options = options.replace(/"(\w+)"\s*:/g, '$1:').trim();
-			options = `, ${options}`;
-		}
-
-		let macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-		if (property === "image") {
-			macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}'}) }}`;
-
-			if (collection === "image") {
-				macro = `{{ cms.imagePath('${id}'${options}) }}`;
-			}
-		}
-
-		if (name) {
-			macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-			if (property === "gallery") {
-				macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}'}) }}`;
-
-				if (collection === "gallery") {
-					macro = `{{ cms.galleryPath('${id}', '${name}'${options}) }}`;
-				}
-			}
-		}
-
 		const macroContent = document.getElementById("twig-macro");
-		macroContent.textContent = macro;
+		macroContent.textContent = MacroBuilder.imageMacro(data);
 	}
 
 	const filesize = document.getElementById('filesize');
