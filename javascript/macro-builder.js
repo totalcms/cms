5b8a29d926bbc7fba77a1a8d7eaa3d8d55e5e9e0
Joe Workman <joe@workmanmail.com>
2024-10-27 23:38:32 -0700

    macro builder class

--- javascript/macro-builder.js
+++ javascript/macro-builder.js
@@ -8,59 +8,100 @@
 **/
 export default class MacroBuilder {
 
-    constructor(button, options={}) {
-    }
+	static buildOptions(data)
+	{
+		// Delete the keys from data
+		delete data.id;
+		delete data.collection;
+		delete data.property;
+		delete data.cache;
+		delete data.name;
 
-	// generateTwigMacro(data) => {
-	// 	const id         = data.id;
-	// 	const collection = data.collection;
-	// 	const property   = data.property;
-	// 	const name       = data.name;
+		// Convert string numbers to actual numbers
+		data = Object.entries(data).reduce((acc, [key, value]) => {
+			acc[key] = isNaN(value) ? value : parseFloat(value);
+			return acc;
+		}, {});
 
-	// 	// Delete the keys from data
-	// 	delete data.id;
-	// 	delete data.collection;
-	// 	delete data.property;
-	// 	delete data.cache;
-	// 	delete data.name;
+		let options = '';
 
-	// 	// Convert string numbers to actual numbers
-	// 	data = Object.entries(data).reduce((acc, [key, value]) => {
-	// 		acc[key] = isNaN(value) ? value : parseFloat(value);
-	// 		return acc;
-	// 	}, {});
+		if (Object.keys(data).length > 0) {
+			options = JSON.stringify(data);
+			options = options.replace(/"(\w+)"\s*:/g, '$1:').trim();
+			options = `, ${options}`;
+		}
 
-	// 	let options = '';
+		return options;
+	}
 
-	// 	if (Object.keys(data).length > 0) {
-	// 		options = JSON.stringify(data);
-	// 		options = options.replace(/"(\w+)"\s*:/g, '$1:').trim();
-	// 		options = `, ${options}`;
-	// 	}
+	static imageMacro(data)
+	{
+		if (data.name) return this.galleryPathMacro(data);
+		return this.imagePathMacro(data);
+	}
 
-	// 	let macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-	// 	if (property === "image") {
-	// 		macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}'}) }}`;
+	static imagePathMacro(data)
+	{
+		if (data.name) return this.galleryPathMacro(data);
 
-	// 		if (collection === "image") {
-	// 			macro = `{{ cms.imagePath('${id}'${options}) }}`;
-	// 		}
-	// 	}
+		const id         = data.id;
+		const collection = data.collection;
+		const property   = data.property;
 
-	// 	if (name) {
-	// 		macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
-	// 		if (property === "gallery") {
-	// 			macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}'}) }}`;
+		const options = this.buildOptions(data);
 
-	// 			if (collection === "gallery") {
-	// 				macro = `{{ cms.galleryPath('${id}', '${name}'${options}) }}`;
-	// 			}
-	// 		}
-	// 	}
+		let macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
+		if (property === "image") {
+			macro = `{{ cms.imagePath('${id}'${options}, {collection:'${collection}'}) }}`;
 
-	// 	const macroContent = document.getElementById("twig-macro");
-	// 	macroContent.textContent = macro;
-	// }
+			if (collection === "image") {
+				macro = `{{ cms.imagePath('${id}'${options}) }}`;
+			}
+		}
+
+		return macro;
+	}
+
+	static galleryPathMacro(data)
+	{
+		const id         = data.id;
+		const collection = data.collection;
+		const property   = data.property;
+		const name       = data.name;
+
+		const options = this.buildOptions(data);
+
+		let macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
+		if (property === "gallery") {
+			macro = `{{ cms.galleryPath('${id}', '${name}'${options}, {collection:'${collection}'}) }}`;
+
+			if (collection === "gallery") {
+				macro = `{{ cms.galleryPath('${id}', '${name}'${options}) }}`;
+			}
+		}
+
+		return macro;
+	}
+
+	static fileDownloadMacro(data)
+	{
+		const id         = data.id;
+		const collection = data.collection;
+		const property   = data.property;
+
+		const options = this.buildOptions(data);
+
+		let macro = `{{ cms.download('${id}'${options}, {collection:'${collection}',property:'${property}'}) }}`;
+		if (property === "file") {
+			macro = `{{ cms.download('${id}'${options}, {collection:'${collection}'}) }}`;
+
+			if (collection === "file") {
+				macro = `{{ cms.download('${id}'${options}) }}`;
+			}
+		}
+
+		return macro;
+	}
 
 }
 
