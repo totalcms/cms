3b56050d60cf107cdff37989015f75ab9fc0c6cf
Joe Workman <joe@workmanmail.com>
2024-11-14 10:24:27 -0800

    depot saver class is saving files and data

--- src/Domain/Property/Service/DepotSaver.php
+++ src/Domain/Property/Service/DepotSaver.php
@@ -2,7 +2,6 @@
 
 namespace TotalCMS\Domain\Property\Service;
 
-use TotalCMS\Component\Depot;
 use TotalCMS\Domain\Object\Data\ObjectData;
 use TotalCMS\Domain\Property\Data\DepotData;
 use TotalCMS\Domain\Property\Data\FileData;
@@ -31,18 +30,11 @@ final class DepotSaver extends FileSaver
 			throw new \RuntimeException('Expected instance of DepotData');
 		}
 
-		$depot = self::appendFileToDepot($depot, $newfile, $subpath);
+		// Directly find or create the folder in the specified path and add the file
+		$folder = &self::findOrCreateFolderByPath($depot->files, $subpath);
+		$folder[] = $newfile;
 
 		return $this->updateObject($collection, $objectID, $property, $depot->transform());
-	}
-
-	private static function appendFileToDepot(DepotData $depot, FileData $newfile, ?string $path) : DepotData
-	{
-		$files        = $depot->files;
-		$folder       = self::findOrCreateFolderByPath($files, $path);
-		$folder[]     = $newfile;
-		$depot->files = $files;
-		return $depot;
 	}
 
 	/**
@@ -52,16 +44,14 @@ final class DepotSaver extends FileSaver
 	 */
 	private static function &findOrCreateFolderByPath(array &$files, ?string $path) : array
 	{
-		 // If path is empty, return the top-level structure
-		 if (empty($path)) {
+		// If path is empty, return the top-level structure
+		if (empty($path)) {
 			return $files;
 		}
 
 		// Break down the path into an array of folder names
 		$pathParts = explode('/', trim($path, '/'));
 		$currentFolderName = array_shift($pathParts);
-
-		echo("Current folder name: " . $currentFolderName);
 
 		// Traverse through the structure to find or create the specified path
 		foreach ($files as &$item) {
@@ -80,10 +70,11 @@ final class DepotSaver extends FileSaver
 
 		// Folder not found, so create it
 		$newFolder = new FolderData($currentFolderName);
-		$files[] = $newFolder;
-		echo("Created new folder: " . $currentFolderName);
+		$files[]   = $newFolder;
 
-		$lastItem = $files[count($files) - 1];
+		// Process the new folder that we just added
+		$lastIndex = count($files) - 1;
+		$lastItem  = $files[$lastIndex];
 		if (!$lastItem instanceof FolderData) {
 			throw new \RuntimeException('Expected instance of FolderData');
 		}
