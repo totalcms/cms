f50f2ba19fd8ca0523753f29a62c4944f036325c
Joe Workman <joe@workmanmail.com>
2024-11-13 22:46:52 -0800

    attempted fix for depot data save

--- src/Domain/Property/Service/DepotSaver.php
+++ src/Domain/Property/Service/DepotSaver.php
@@ -3,6 +3,7 @@
 namespace TotalCMS\Domain\Property\Service;
 
 use TotalCMS\Domain\Object\Data\ObjectData;
+use TotalCMS\Domain\Property\Data\DepotData;
 use TotalCMS\Domain\Property\Data\FileData;
 use TotalCMS\Domain\Property\Data\FolderData;
 
@@ -21,69 +22,88 @@ final class DepotSaver extends FileSaver
 			$this->createObject($collection, $objectID, $property);
 		}
 
-		$files    = $this->fetchProperty($collection, $objectID, $property)->transform();
 		$fileinfo = $this->storage->saveFile($collection, $objectID, $property, $filePath, $subpath);
+		$newfile  = new FileData($fileinfo);
+		$depot    = $this->fetchProperty($collection, $objectID, $property);
 
-		$folder   = self::findOrCreateFolderByPath($files, $subpath);
-		$folder[] = (new FileData($fileinfo))->transform();
+		$depot = self::appendFileToDepot($depot, $newfile, $subpath);
 
-		return $this->updateObject($collection, $objectID, $property, $files);
+		// $folder   = self::findOrCreateFolderByPath($depot, $subpath);
+		// $folder[] = (new FileData($fileinfo))->transform();
+
+		return $this->updateObject($collection, $objectID, $property, $depot->transform());
 	}
 
-	public function newFolder(
-		string $collection,
-		string $objectID,
-		string $property,
-		string $folderName,
-		?string $subpath = null
-	): ObjectData
+	// public function newFolder(
+	// 	string $collection,
+	// 	string $objectID,
+	// 	string $property,
+	// 	string $folderName,
+	// 	?string $subpath = null
+	// ): ObjectData
+	// {
+	// 	$objectExists = $this->objectFetcher->existsObject($collection, $objectID);
+	// 	if (!$objectExists) {
+	// 		$this->createObject($collection, $objectID, $property);
+	// 	}
+
+	// 	$files = $this->fetchProperty($collection, $objectID, $property)->transform();
+
+	// 	$folder   = self::findOrCreateFolderByPath($files, $subpath);
+	// 	$folder[] = (new FolderData($folderName))->transform();
+
+	// 	return $this->updateObject($collection, $objectID, $property, $files);
+	// }
+
+	private static function appendFileToDepot(DepotData $depot, FileData $newfile, ?string $path) : DepotData
 	{
-		$objectExists = $this->objectFetcher->existsObject($collection, $objectID);
-		if (!$objectExists) {
-			$this->createObject($collection, $objectID, $property);
-		}
+		$files        = $depot->files;
+		$folder       = self::findOrCreateFolderByPath($files, $path);
+		$folder[]     = $newfile;
+		$depot->files = $files;
 
-		$files = $this->fetchProperty($collection, $objectID, $property)->transform();
-
-		$folder   = self::findOrCreateFolderByPath($files, $subpath);
-		$folder[] = (new FolderData($folderName))->transform();
-
-		return $this->updateObject($collection, $objectID, $property, $files);
+		// $depot->files[] = $newfile;
+		return $depot;
 	}
 
 	/**
-	 * @param array<mixed> $structure
+	 * @param array<FolderData|FileData> $files
 	 *
-	 * @return array<mixed>
+	 * @return array<FolderData|FileData>
 	 */
-	private static function findOrCreateFolderByPath(array &$structure, ?string $path) : array
+	private static function findOrCreateFolderByPath(array &$files, ?string $path) : array
 	{
 		 // If path is empty, return the top-level structure
 		 if (empty($path)) {
-			return $structure;
+			return $files;
 		}
 
 		// Break down the path into an array of folder names
 		$pathParts = explode('/', trim($path, '/'));
 		$currentFolderName = array_shift($pathParts);
 
+		echo("Current folder name: " . $currentFolderName);
+
 		// Traverse through the structure to find or create the specified path
-		foreach ($structure as &$item) {
-			if (isset($item['mime']) && $item['mime'] === 'folder' && $item['name'] === $currentFolderName) {
+		foreach ($files as &$item) {
+			if (!($item instanceof FolderData)) {
+				continue;
+			}
+			if ($item->name === $currentFolderName) {
 				// If this is the final part of the path, return this folder
 				if (empty($pathParts)) {
-					return $item['files'];
+					return $item->files;
 				}
 				// Recurse into the existing folder
-				return self::findOrCreateFolderByPath($item['files'], implode('/', $pathParts));
+				return self::findOrCreateFolderByPath($item->files, implode('/', $pathParts));
 			}
 		}
 
 		// Folder not found, so create it
-		$newFolder = (new FolderData($currentFolderName))->transform();
-		$structure[] = $newFolder;
+		$newFolder = new FolderData($currentFolderName);
+		$files[] = $newFolder;
+		echo("Created new folder: " . $currentFolderName);
 
-		// Recursively create the rest of the path in the new folder
-		return self::findOrCreateFolderByPath($structure[count($structure) - 1]['files'], implode('/', $pathParts));
+		return self::findOrCreateFolderByPath($files, $currentFolderName);
 	}
 }
