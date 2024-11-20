dfa80f64794b463db598a256ad772a2bcbafc2c9
Joe Workman <joe@workmanmail.com>
2024-11-19 23:45:43 -0800

    more depot form improvements

--- public/test/depot.php
+++ public/test/depot.php
@@ -6,6 +6,7 @@
 
 <form class="totalform" style="margin-top:2rem">
 	<div class="depot-layout">
+		<div class="depot-browser-wrapper">
 		<ul class="depot-browser">
 			<li>
 				<details open="">
@@ -15,25 +16,144 @@
 							<details open="">
 								<summary class="folder">another-folder</summary>
 								<ul class="folder-contents">
-									<li class="file">BrazilHeart-small-673d36552320b.png</li>
-									<li class="file">BrazilHeart-small-673d3655ca873.png</li>
-									<li class="file">BrazilHeart-small.png</li>
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+										<div class="size">3MB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+										<div class="size">200KB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+										<div class="size">3.5MB</div>
+									</li>
 								</ul>
 							</details>
 						</li>
-						<li class="file">BrazilHeart-small-673d323dccd8d.png</li>
-						<li class="file">BrazilHeart-small-673d323e68ead.png</li>
-						<li class="file">BrazilHeart-small-673d323ed9109.png</li>
-						<li class="file">BrazilHeart-small.png</li>
+						<li>
+							<details open="">
+								<summary class="folder">and-another-folder</summary>
+								<ul class="folder-contents">
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+										<div class="size">3MB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+										<div class="size">200KB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+										<div class="size">3.5MB</div>
+									</li>
+								</ul>
+							</details>
+						</li>
+						<li>
+							<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+							<div class="size">3MB</div>
+						</li>
+						<li>
+							<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+							<div class="size">200KB</div>
+						</li>
+						<li>
+							<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+							<div class="size">3.5MB</div>
+						</li>
 					</ul>
 				</details>
 			</li>
-			<li class="file">BrazilHeart-small-673d3aefdd486.png</li>
-			<li class="file">BrazilHeart-small-673d3af0e459f.png</li>
-			<li class="file">BrazilHeart-small.png</li>
+			<li>
+				<details open="">
+					<summary class="folder">subfolder</summary>
+					<ul class="folder-contents">
+						<li>
+							<details open="">
+								<summary class="folder">another-folder</summary>
+								<ul class="folder-contents">
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+										<div class="size">3MB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+										<div class="size">200KB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+										<div class="size">3.5MB</div>
+									</li>
+								</ul>
+							</details>
+						</li>
+						<li>
+							<details open="">
+								<summary class="folder">and-another-folder</summary>
+								<ul class="folder-contents">
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+										<div class="size">3MB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+										<div class="size">200KB</div>
+									</li>
+									<li>
+										<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+										<div class="size">3.5MB</div>
+									</li>
+								</ul>
+							</details>
+						</li>
+						<li>
+							<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+							<div class="size">3MB</div>
+						</li>
+						<li>
+							<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+							<div class="size">200KB</div>
+						</li>
+						<li>
+							<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+							<div class="size">3.5MB</div>
+						</li>
+					</ul>
+				</details>
+			</li>
+			<li>
+				<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
+				<div class="size">3MB</div>
+			</li>
+			<li>
+				<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
+				<div class="size">200KB</div>
+			</li>
+			<li>
+				<div class="file file-icon icon-zip">BrazilHeart-small.png</div>
+				<div class="size">3.5MB</div>
+			</li>
 		</ul>
+		</div>
 		<div class="depot-preview">
-			<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 64 64"><g fill="#222222"><path d="M17,4H10a5.937,5.937,0,0,0-6,6v7a1,1,0,0,0,2,0V10a3.957,3.957,0,0,1,4-4h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M54,4H47a1,1,0,0,0,0,2h7a3.957,3.957,0,0,1,4,4v7a1,1,0,0,0,2,0V10A5.937,5.937,0,0,0,54,4Z" fill="#222222"></path><path d="M59,46a1,1,0,0,0-1,1v7a3.957,3.957,0,0,1-4,4H47a1,1,0,0,0,0,2h7a5.937,5.937,0,0,0,6-6V47A1,1,0,0,0,59,46Z" fill="#222222"></path><path d="M17,58H10a3.957,3.957,0,0,1-4-4V47a1,1,0,0,0-2,0v7a5.937,5.937,0,0,0,6,6h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M25,6H39a1,1,0,0,0,0-2H25a1,1,0,0,0,0,2Z" fill="#222222"></path><path d="M39,58H25a1,1,0,0,0,0,2H39a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M59,24a1,1,0,0,0-1,1V39a1,1,0,0,0,2,0V25A1,1,0,0,0,59,24Z" fill="#222222"></path><path d="M5,40a1,1,0,0,0,1-1V25a1,1,0,0,0-2,0V39A1,1,0,0,0,5,40Z" fill="#222222"></path><path d="M32.781,15.375a1.036,1.036,0,0,0-1.562,0l-12,15A1,1,0,0,0,20,32h9V46a3,3,0,0,0,6,0V32h9a1,1,0,0,0,.781-1.625Z" fill="#222222"></path></g></svg>
+			<!-- <div class="folder-upload">
+				<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 64 64"><g fill="#222222"><path d="M17,4H10a5.937,5.937,0,0,0-6,6v7a1,1,0,0,0,2,0V10a3.957,3.957,0,0,1,4-4h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M54,4H47a1,1,0,0,0,0,2h7a3.957,3.957,0,0,1,4,4v7a1,1,0,0,0,2,0V10A5.937,5.937,0,0,0,54,4Z" fill="#222222"></path><path d="M59,46a1,1,0,0,0-1,1v7a3.957,3.957,0,0,1-4,4H47a1,1,0,0,0,0,2h7a5.937,5.937,0,0,0,6-6V47A1,1,0,0,0,59,46Z" fill="#222222"></path><path d="M17,58H10a3.957,3.957,0,0,1-4-4V47a1,1,0,0,0-2,0v7a5.937,5.937,0,0,0,6,6h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M25,6H39a1,1,0,0,0,0-2H25a1,1,0,0,0,0,2Z" fill="#222222"></path><path d="M39,58H25a1,1,0,0,0,0,2H39a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M59,24a1,1,0,0,0-1,1V39a1,1,0,0,0,2,0V25A1,1,0,0,0,59,24Z" fill="#222222"></path><path d="M5,40a1,1,0,0,0,1-1V25a1,1,0,0,0-2,0V39A1,1,0,0,0,5,40Z" fill="#222222"></path><path d="M32.781,15.375a1.036,1.036,0,0,0-1.562,0l-12,15A1,1,0,0,0,20,32h9V46a3,3,0,0,0,6,0V32h9a1,1,0,0,0,.781-1.625Z" fill="#222222"></path></g></svg>
+				<div>another-folder</div>
+			</div> -->
+			<div class="file-preview">
+				<div class="file file-icon icon-zip"></div>
+				<div>BrazilHeart.zip - 3MB</div>
+				<div>2024/11/22 02:30PM</div>
+			</div>
+			<div class="actionbar">
+				<button type="button" class="edit" title="Edit File Info"></button>
+				<button type="button" class="links" title="Download Links"></button>
+				<button type="button" class="download" title="Download File"></button>
+				<button type="button" class="upload dz-clickable" title="Upload"></button>
+				<button type="button" class="add-folder" title="New Folder"></button>
+				<button type="button" class="trash" title="Delete File"></button>
+			</div>
 		</div>
 	</div>
 </form>
