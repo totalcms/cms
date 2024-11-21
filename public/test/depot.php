98c46b97981b97131a9d86afe4c5d5969e568659
Joe Workman <joe@workmanmail.com>
2024-11-20 16:26:45 -0800

    depot folder selected view

--- public/test/depot.php
+++ public/test/depot.php
@@ -15,7 +15,7 @@
 					<ul class="folder-contents">
 						<li>
 							<details open="">
-								<summary class="folder">another-folder</summary>
+								<summary class="folder selected">another-folder</summary>
 								<ul class="folder-contents">
 									<li>
 										<div class="file file-icon icon-zip">BrazilHeart-small-673d36552320b.png</div>
@@ -25,7 +25,7 @@
 										<div class="file file-icon icon-png">BrazilHeart-small-673d365.png</div>
 										<div class="size">200KB</div>
 									</li>
-									<li class="selected">
+									<li>
 										<div class="file file-icon icon-zip">BrazilHeart.zip</div>
 										<div class="size">3.5MB</div>
 									</li>
@@ -138,11 +138,11 @@
 		</ul>
 		</div>
 		<div class="depot-preview">
-			<!-- <div class="folder-upload">
+			<div class="folder-upload">
 				<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 64 64"><g fill="#222222"><path d="M17,4H10a5.937,5.937,0,0,0-6,6v7a1,1,0,0,0,2,0V10a3.957,3.957,0,0,1,4-4h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M54,4H47a1,1,0,0,0,0,2h7a3.957,3.957,0,0,1,4,4v7a1,1,0,0,0,2,0V10A5.937,5.937,0,0,0,54,4Z" fill="#222222"></path><path d="M59,46a1,1,0,0,0-1,1v7a3.957,3.957,0,0,1-4,4H47a1,1,0,0,0,0,2h7a5.937,5.937,0,0,0,6-6V47A1,1,0,0,0,59,46Z" fill="#222222"></path><path d="M17,58H10a3.957,3.957,0,0,1-4-4V47a1,1,0,0,0-2,0v7a5.937,5.937,0,0,0,6,6h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M25,6H39a1,1,0,0,0,0-2H25a1,1,0,0,0,0,2Z" fill="#222222"></path><path d="M39,58H25a1,1,0,0,0,0,2H39a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M59,24a1,1,0,0,0-1,1V39a1,1,0,0,0,2,0V25A1,1,0,0,0,59,24Z" fill="#222222"></path><path d="M5,40a1,1,0,0,0,1-1V25a1,1,0,0,0-2,0V39A1,1,0,0,0,5,40Z" fill="#222222"></path><path d="M32.781,15.375a1.036,1.036,0,0,0-1.562,0l-12,15A1,1,0,0,0,20,32h9V46a3,3,0,0,0,6,0V32h9a1,1,0,0,0,.781-1.625Z" fill="#222222"></path></g></svg>
-				<div>another-folder</div>
-			</div> -->
-			<div class="file-preview">
+				<h4>another-folder</h4>
+			</div>
+			<!-- <div class="file-preview">
 				<div class="file file-icon icon-zip">
 					<h4>BrazilHeart.png</h4>
 				</div>
@@ -166,11 +166,11 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> -->
 			<div class="actionbar">
 				<button type="button" class="edit" title="Edit File Info"></button>
-				<button type="button" class="links" title="Download Links"></button>
-				<button type="button" class="download" title="Download File"></button>
+				<button type="button" disabled class="links" title="Download Links"></button>
+				<button type="button" disabled class="download" title="Download File"></button>
 				<button type="button" class="upload dz-clickable" title="Upload"></button>
 				<button type="button" class="add-folder" title="New Folder"></button>
 				<button type="button" class="trash" title="Delete File"></button>
