927c4676d39f3a4cb87a5b572f64ae39c924cb1a
Joe Workman <joe@workmanmail.com>
2024-11-20 20:32:08 -0800

    better depot builder class and fixes

--- public/test/depot.php
+++ public/test/depot.php
@@ -2,9 +2,9 @@
 
 <h1>Total CMS Depot Form Demo</h1>
 
-<!-- {{ cms.form.depot("mydepot") }} -->
+{{ cms.form.depot("mydepot") }}
 
-<form class="totalform" style="margin-top:2rem">
+<!-- <form class="totalform" style="margin-top:2rem">
 	<div class="depot-layout-container">
 	<div class="depot-layout">
 		<div class="depot-browser-wrapper">
@@ -138,10 +138,10 @@
 		</ul>
 		</div>
 		<div class="depot-preview">
-			<!-- <div class="folder-upload">
+			<div class="folder-preview">
 				<svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 64 64"><g fill="#222222"><path d="M17,4H10a5.937,5.937,0,0,0-6,6v7a1,1,0,0,0,2,0V10a3.957,3.957,0,0,1,4-4h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M54,4H47a1,1,0,0,0,0,2h7a3.957,3.957,0,0,1,4,4v7a1,1,0,0,0,2,0V10A5.937,5.937,0,0,0,54,4Z" fill="#222222"></path><path d="M59,46a1,1,0,0,0-1,1v7a3.957,3.957,0,0,1-4,4H47a1,1,0,0,0,0,2h7a5.937,5.937,0,0,0,6-6V47A1,1,0,0,0,59,46Z" fill="#222222"></path><path d="M17,58H10a3.957,3.957,0,0,1-4-4V47a1,1,0,0,0-2,0v7a5.937,5.937,0,0,0,6,6h7a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M25,6H39a1,1,0,0,0,0-2H25a1,1,0,0,0,0,2Z" fill="#222222"></path><path d="M39,58H25a1,1,0,0,0,0,2H39a1,1,0,0,0,0-2Z" fill="#222222"></path><path d="M59,24a1,1,0,0,0-1,1V39a1,1,0,0,0,2,0V25A1,1,0,0,0,59,24Z" fill="#222222"></path><path d="M5,40a1,1,0,0,0,1-1V25a1,1,0,0,0-2,0V39A1,1,0,0,0,5,40Z" fill="#222222"></path><path d="M32.781,15.375a1.036,1.036,0,0,0-1.562,0l-12,15A1,1,0,0,0,20,32h9V46a3,3,0,0,0,6,0V32h9a1,1,0,0,0,.781-1.625Z" fill="#222222"></path></g></svg>
 				<h4 class="folder-name">another-folder</h4>
-			</div> -->
+			</div>
 			<div class="file-preview">
 				<div class="file file-icon icon-zip">
 					<h4 class="file-name">BrazilHeart.png</h4>
@@ -178,7 +178,7 @@
 		</div>
 	</div>
 	</div>
-</form>
+</form> -->
 
 <!-- <p>Max file upload size: <?php echo ini_get('upload_max_filesize'); ?></p>
 
