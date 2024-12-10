685c3732c0ad870820d59ff5fdeee6cced9fb580
Joe Workman <joe@workmanmail.com>
2024-12-10 10:42:03 -0800

    fixed Sentry

--- src/Handler/DefaultErrorHandler.php
+++ src/Handler/DefaultErrorHandler.php
@@ -69,6 +69,9 @@ final class DefaultErrorHandler
 			);
 		}
 
+		// Integrate with Sentry
+		\Sentry\captureException($exception);
+
 		// Detect status code
 		$statusCode = $this->getHttpStatusCode($exception);
 
