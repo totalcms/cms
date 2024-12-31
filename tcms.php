7f97634754ac7ca9865a33dc2240b7e5e5434b19
Joe Workman <joe@workmanmail.com>
2024-12-30 21:21:35 -0800

    example tcms.php

---
+++ tcms.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+	// "watermarksGallery" => "watermarks",
+	// "timezone"          => "America/Denver",
+	// "sentry"            => true,
+	// "presets"           => [
+	// 	"small" => [
+	// 		"w"   => 300,
+	// 		"h"   => 200,
+	// 	],
+	// 	"small-crop" => [
+	// 		"w"   => 300,
+	// 		"h"   => 300,
+	// 		"fit" => "crop-focalpoint",
+	// 	],
+	// 	"medium" => [
+	// 		"w"   => 600,
+	// 		"h"   => 400,
+	// 	],
+	// 	"medium-crop" => [
+	// 		"w"   => 600,
+	// 		"h"   => 600,
+	// 		"fit" => "crop-focalpoint",
+	// 	],
+	// ]
+];
