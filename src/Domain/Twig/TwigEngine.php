3c95a2247466b1940fe4a77109d828a4ee6471e4
Joe Workman <joe@workmanmail.com>
2025-05-05 14:49:40 -0700

    new extensions!

--- src/Domain/Twig/TwigEngine.php
+++ src/Domain/Twig/TwigEngine.php
@@ -7,8 +7,11 @@ use TotalCMS\Domain\Template\Repository\TemplateRepository;
 use TotalCMS\Support\Config;
 use Twig\Environment as TwigEnvironment;
 use Twig\Extension\DebugExtension;
+use Twig\Extra\Html\HtmlExtension;
+use Twig\Extra\Intl\IntlExtension;
+use Twig\Extra\Markdown\MarkdownExtension;
+use Twig\Extra\String\StringExtension;
 use Twig\Loader\FilesystemLoader as TwigFilesystemLoader;
-use Twig\Runtime\EscaperRuntime;
 
 /**
  * Twig template processor.
@@ -38,7 +41,13 @@ final class TwigEngine
 			'debug'      => $debug,
 			'autoescape' => false,
 		]);
+
 		$this->twig->addExtension($extension);
+		$this->twig->addExtension(new StringExtension());
+		$this->twig->addExtension(new IntlExtension());
+		$this->twig->addExtension(new HtmlExtension());
+		$this->twig->addExtension(new MarkdownExtension());
+
 		if ($debug) {
 			$this->twig->addExtension(new DebugExtension());
 		}
