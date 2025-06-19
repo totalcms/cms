14b3f18f5ffef43c2aaa84422c861499e2bb69ca
Joe Workman <joe@workmanmail.com>
2025-06-19 12:12:15 -0700

    better slugify IDs

--- javascript/totalform/identifier.js
+++ javascript/totalform/identifier.js
@@ -93,7 +93,14 @@ export default class Identifier extends TotalField {
 
     slugify(id) {
 		id = id.replace('@', '-at-').replace('.', '-');
-        return slugify(id, { lower: true });
+        return slugify(id, {
+			replacement : '-', // replace spaces with replacement character, defaults to `-`
+			remove      : /[*+~.()'"!:@]/g, // remove characters that match regex, defaults to `undefined`
+			lower       : true, // convert to lower case, defaults to `false`
+			strict      : false, // strip special characters except replacement, defaults to `false`
+			trim        : true, // trim leading and trailing replacement chars, defaults to `true`
+			// locale      : 'vi', // language code of the locale to use
+		});
     }
 
     idExists() {
