3f588d09dfff410709e0c0f7adda74d1aa36c07b
Joe Workman <joe@workmanmail.com>
2025-06-23 20:07:06 -0700

    Export Collection to Zip!

--- config/routes/export.php
+++ config/routes/export.php
@@ -8,6 +8,7 @@ return function (App $app) {
 	$app->group('/export', function (RouteCollectorProxy $group) {
 		$group->get('/collections/{collection}[/json]', Export\ExportJsonAction::class)->setName('export-json');
 		$group->get('/collections/{collection}/csv', Export\ExportCsvAction::class)->setName('export-csv');
+		$group->get('/collections/{collection}/zip', Export\ExportZipAction::class)->setName('export-zip');
 		$group->get('/schemas/{schema}', Export\ExportSchemaAction::class)->setName('export-schema');
 	});
 };
