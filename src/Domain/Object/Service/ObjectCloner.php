29167f95c98ff7a4d9c2677dc1718b326ad235a5
Joe Workman <joe@workmanmail.com>
2025-07-25 13:03:59 -0700

    clone objects now update onCreate and onUpdate dates

--- src/Domain/Object/Service/ObjectCloner.php
+++ src/Domain/Object/Service/ObjectCloner.php
@@ -5,31 +5,20 @@ namespace TotalCMS\Domain\Object\Service;
 use TotalCMS\Domain\Index\Service\IndexBuilder;
 use TotalCMS\Domain\Object\Data\ObjectData;
 use TotalCMS\Domain\Object\Repository\ObjectRepository;
+use TotalCMS\Domain\Property\Data\DateData;
+use TotalCMS\Domain\Schema\Service\SchemaFetcher;
 
-/**
- * Service.
- */
 final class ObjectCloner
 {
-	private ObjectRepository $storage;
-	private IndexBuilder $indexBuilder;
-
-	public function __construct(ObjectRepository $storage, IndexBuilder $indexBuilder)
-	{
-		$this->storage      = $storage;
-		$this->indexBuilder = $indexBuilder;
-	}
+	public function __construct(
+		private ObjectRepository $storage,
+		private IndexBuilder $indexBuilder,
+		private SchemaFetcher $schemaFetcher
+	){}
 
 	/**
-	 * save a collection object.
-	 *
 	 * @param array<string,mixed> $from
 	 * @param array<string,mixed> $to
-	 *
-	 * @throws \UnexpectedValueException
-	 * @throws \RuntimeException
-	 *
-	 * @return ObjectData
 	 */
 	public function cloneObject(array $from, array $to): ObjectData
 	{
@@ -44,6 +33,10 @@ final class ObjectCloner
 			throw new \DomainException(sprintf('Object with id %s already exists in %s', $to['id'], $to['collection']));
 		}
 
+		// Reset onCreate and onUpdate date fields to current time
+		$this->resetOnCreateDateFields($object, $to['collection']);
+		$this->resetOnUpdateDateFields($object, $to['collection']);
+
 		$this->storage->saveObject($to['collection'], $object);
 
 		$this->storage->copyObjectFiles($from['collection'], $from['id'], $to['collection'], $to['id']);
@@ -52,5 +45,60 @@ final class ObjectCloner
 		$this->indexBuilder->smartBuildIndex($to['collection'], $object);
 
 		return $object;
+	}
+
+	private function resetOnCreateDateFields(ObjectData $object, string $collection): void
+	{
+		$onCreateFields = $this->getDateFieldsBySetting($collection, DateData::CREATION_DATE);
+		$this->resetDateFields($object, $onCreateFields, 'onCreate');
+	}
+
+	private function resetOnUpdateDateFields(ObjectData $object, string $collection): void
+	{
+		$onUpdateFields = $this->getDateFieldsBySetting($collection, DateData::UPDATE_DATE);
+		$this->resetDateFields($object, $onUpdateFields, 'onUpdate');
+	}
+
+	/**
+	 * @param array<string> $fields
+	 */
+	private function resetDateFields(ObjectData $object, array $fields, string $settingType): void
+	{
+		$currentDate = DateData::cleanDate();
+
+		error_log("ObjectCloner: Found {$settingType} fields: " . implode(', ', $fields));
+
+		foreach ($fields as $fieldName) {
+			if ($object->properties->has($fieldName) && $object->properties->get($fieldName) instanceof DateData) {
+				/** @var DateData $dateProperty */
+				$dateProperty = $object->properties->get($fieldName);
+				$dateProperty->date = $currentDate;
+			}
+		}
+	}
+
+	/** @return array<string>  */
+	private function getDateFieldsBySetting(string $collection, string $setting): array
+	{
+		$schema = $this->schemaFetcher->fetchSchemaForCollection($collection);
+		$dateFields = [];
+
+		foreach ($schema->properties as $fieldName => $fieldDefinition) {
+			// Check if this is a date field with the specified setting
+			$isDateField = (
+				(isset($fieldDefinition['type']) && $fieldDefinition['type'] === 'date') ||
+				(isset($fieldDefinition['$ref']) && str_contains($fieldDefinition['$ref'], '/date.json'))
+			);
+
+			$hasSetting = (
+				(isset($fieldDefinition[$setting]) && $fieldDefinition[$setting] === true) ||
+				(isset($fieldDefinition['settings'][$setting]) && $fieldDefinition['settings'][$setting] === true)
+			);
+
+			if ($isDateField && $hasSetting) {
+				$dateFields[] = $fieldName;
+			}
+		}
+		return $dateFields;
 	}
 }
