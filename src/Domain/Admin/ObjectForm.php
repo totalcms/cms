9c5c48ca9cd76ea6a51976291cb8a7aebff52969
Joe Workman <joe@workmanmail.com>
2025-08-24 15:13:13 -0700

    phpstan fix

--- src/Domain/Admin/ObjectForm.php
+++ src/Domain/Admin/ObjectForm.php
@@ -61,7 +61,7 @@ final class ObjectForm extends TotalForm
 
 			// if the value is not already set, try to get it from the object data
 			if (!isset($options['value']) && isset($this->objectData)) {
-				$value = $this->objectData->toArray()[$name] ?? '';
+				$value = $this->objectData->toArray()[$name] ?? null;
 				// Use strict checks to preserve zero values (0, 0.0, '0')
 				if ($value !== '' && $value !== null) {
 					$options['value'] = $value;
