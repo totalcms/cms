c9414542e20309e9b33aff40eb3d8c9c819175dc
Joe Workman <joe@workmanmail.com>
2025-09-27 19:36:54 -0700

    Revert "deep merge settings"
    This reverts commit cb6e8ddbb5ebc5c5e00d8f2015b0f8a9f598920a.

--- config/settings.php
+++ config/settings.php
@@ -18,24 +18,6 @@ if ($environment) {
 	}
 }
 
-/**
- * Recursively merge arrays, giving precedence to the second array.
- */
-function deepMergeArrays(array $array1, array $array2): array
-{
-	$merged = $array1;
-
-	foreach ($array2 as $key => $value) {
-		if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
-			$merged[$key] = deepMergeArrays($merged[$key], $value);
-		} else {
-			$merged[$key] = $value;
-		}
-	}
-
-	return $merged;
-}
-
 // User defined settings
 if (file_exists(($_SERVER['DOCUMENT_ROOT'] ?? '') . '/tcms.php')) {
 	$userSettings = require ($_SERVER['DOCUMENT_ROOT'] ?? '') . '/tcms.php';
@@ -47,8 +29,6 @@ if (file_exists(($_SERVER['DOCUMENT_ROOT'] ?? '') . '/tcms.php')) {
 			'watermarksGallery'   => 'imageworks/watermarksGallery',
 			'watermarkFontsDepot' => 'imageworks/watermarkFontsDepot',
 		];
-
-		// Handle mapped settings first
 		foreach ($userSettings as $key => $value) {
 			if (isset($userSettingsMap[$key])) {
 				$keys = explode('/', $userSettingsMap[$key]);
@@ -61,12 +41,10 @@ if (file_exists(($_SERVER['DOCUMENT_ROOT'] ?? '') . '/tcms.php')) {
 					$temp = &$temp[$key];
 				}
 				$temp = $value;
-				unset($userSettings[$key]); // Remove from userSettings to avoid double processing
+				continue;
 			}
+			$settings[$key] = $value;
 		}
-
-		// Deep merge remaining settings
-		$settings = deepMergeArrays($settings, $userSettings);
 	}
 }
 
--- config/tcms-sample.php
+++ config/tcms-sample.php
@@ -1,57 +1,33 @@
 <?php
 
 // Sample configuration file for /tcms.php
-// Note: Total CMS now does deep merging, so you can override specific nested settings
-// without replacing entire configuration arrays.
 
-// Return an array for deep merging (recommended)
-return [
-	'sentry' => [
-		'enable' => true,
+$settings['sentry']['enable'] = true;
+
+$settings['api']     = '/site-assets/stacks/ws.tcms.core/tcms/';
+$settings['datadir'] = $settings['docroot'] . '/tcms-data';
+
+// https://www.php.net/manual/en/timezones.php
+$settings['timezone'] = 'America/Denver';
+
+$settings['imageworks']['watermarksGallery'] = 'watermarks';
+$settings['imageworks']['presets']           = [
+	'small' => [
+		'w'   => 300,
+		'h'   => 200,
 	],
-
-	'api'     => '/site-assets/stacks/ws.tcms.core/tcms/',
-	'datadir' => __DIR__ . '/tcms-data',
-	'timezone' => 'America/Denver', // https://www.php.net/manual/en/timezones.php
-
-	// Cache configuration with deep merge support
-	'cache' => [
-		'redis' => [
-			'enabled'  => true,
-			'password' => 'your_redis_password_here', // Only override the password
-			// Other redis settings (host, port, etc.) will remain from defaults
-		],
-		'memcached' => [
-			'enabled' => false, // Can disable specific cache backends
-		],
+	'small-crop' => [
+		'w'   => 300,
+		'h'   => 300,
+		'fit' => 'crop-focalpoint',
 	],
-
-	// ImageWorks settings
-	'imageworks' => [
-		'watermarksGallery' => 'watermarks',
-		'presets' => [
-			'small' => [
-				'w' => 300,
-				'h' => 200,
-			],
-			'small-crop' => [
-				'w'   => 300,
-				'h'   => 300,
-				'fit' => 'crop-focalpoint',
-			],
-			'medium' => [
-				'w' => 600,
-				'h' => 400,
-			],
-			'medium-crop' => [
-				'w'   => 600,
-				'h'   => 600,
-				'fit' => 'crop-focalpoint',
-			],
-		],
+	'medium' => [
+		'w'   => 600,
+		'h'   => 400,
+	],
+	'medium-crop' => [
+		'w'   => 600,
+		'h'   => 600,
+		'fit' => 'crop-focalpoint',
 	],
 ];
-
-// Legacy style still works but is not recommended:
-// $settings['sentry']['enable'] = true;
-// $settings['cache']['redis']['password'] = 'password';
