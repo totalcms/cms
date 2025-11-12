c7c23e64ad62baf97773b0dbe2fdbc30b63affc8
Joe Workman <joe@workmanmail.com>
2025-11-12 06:45:49 -0800

    code clean

--- src/Domain/Admin/ObjectForm.php
+++ src/Domain/Admin/ObjectForm.php
@@ -19,8 +19,10 @@ class ObjectForm extends TotalForm
 
 		$this->route = "/collections/{$this->collection}";
 
+		$objectExists = $this->objectFetcher->existsObject($this->collection, $this->id);
+
 		// For addOnly forms, never load existing objects even if an ID is somehow present
-		if (!$this->addOnly && $this->id !== '' && $this->objectFetcher->existsObject($this->collection, $this->id)) {
+		if (!$this->addOnly && $this->id !== '' && $objectExists) {
 			// If the form is for editing an existing item, change the method to PUT
 			$this->objectData = $this->objectFetcher->fetchObject($this->collection, $this->id);
 			$this->route      = "/collections/{$this->collection}/{$this->id}";
