4815b634584591931b7aa247037e0a2bb8a64cb4
Joe Workman <joe@workmanmail.com>
2025-12-23 12:52:28 -0800

    fixed project setup

--- src/Action/Admin/AdminUtilsAction.php
+++ src/Action/Admin/AdminUtilsAction.php
@@ -50,7 +50,8 @@ readonly class AdminUtilsAction
 			$page = $args['page'] ?? 'index';
 		}
 
-		$action  = $args['action'] ?? '';
+		$query   = $request->getQueryParams();
+		$action  = $args['action'] ?? $query['action'] ?? '';
 		$results = '';
 
 		if ($request->getMethod() === 'POST') {
