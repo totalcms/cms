15836216be34d9e6b321cdc0f4e2f7f0b4774015
Joe Workman <joe@workmanmail.com>
2026-03-05 07:56:58 -0800

    launch edit actions on Depot Drop form

--- javascript/totalform/depot-drop.js
+++ javascript/totalform/depot-drop.js
@@ -39,6 +39,7 @@ export default class DepotDropField extends TotalField {
         this.dropzone.on("uploadprogress", (file, progress) => this.onProgress(file, progress));
         this.dropzone.on("success", (file, response) => this.onSuccess(file, response));
         this.dropzone.on("error", (file, message) => this.onError(file, message));
+        this.dropzone.on("queuecomplete", () => this.onQueueComplete());
         this.dropzone.on("dragenter", () => this.container.classList.add("dz-drag-hover"));
         this.dropzone.on("dragleave", () => this.container.classList.remove("dz-drag-hover"));
         this.dropzone.on("drop", () => this.container.classList.remove("dz-drag-hover"));
@@ -101,6 +102,13 @@ export default class DepotDropField extends TotalField {
 
         file.previewElement.classList.remove("dz-processing");
         file.previewElement.classList.add("dz-success", "dz-complete");
+        this.hadSuccess = true;
+    }
+
+    onQueueComplete() {
+        if (!this.hadSuccess || !this.form) return;
+        this.hadSuccess = false;
+        this.form.runEditActions();
     }
 
     onError(file, message) {
