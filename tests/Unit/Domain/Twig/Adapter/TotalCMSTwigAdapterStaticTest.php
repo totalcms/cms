798b2a93607d3ea9a6488815ae5a759502a9e874
Joe Workman <joe@workmanmail.com>
2026-06-03 23:50:27 -0700

    Fix AdminTwigAdapter test constructors for new JobQueueHealth dependency
    The job-queue-stalled warning added JobQueueHealth as a constructor arg to
    AdminTwigAdapter; two adapter tests build it directly and still passed 19
    of 20 args. Add the JobQueueHealth mock.

--- tests/Unit/Domain/Twig/Adapter/TotalCMSTwigAdapterStaticTest.php
+++ tests/Unit/Domain/Twig/Adapter/TotalCMSTwigAdapterStaticTest.php
@@ -503,6 +503,7 @@ final class TotalCMSTwigAdapterStaticTest extends TestCase
 			$this->createMock(\TotalCMS\Domain\Builder\Service\BuilderConfigService::class),
 			$this->createMock(\TotalCMS\Domain\Collection\Service\CollectionFetcher::class),
 			$this->createMock(\TotalCMS\Domain\Builder\Service\BuilderTemplatePaths::class),
+			$this->createMock(\TotalCMS\Domain\JobQueue\Service\JobQueueHealth::class),
 		);
 
 		// Mock $_SERVER for test
